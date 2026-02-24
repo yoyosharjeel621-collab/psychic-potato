907b21a617a02697fb3c3e938c19c8138c4aad6d
yoyosharjeel621-collab <yoyosharjeel621@gmail.com>
2026-02-24 12:26:46 +0500

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
 
+// Signup
+router.post("/signup", async (req, res) => {
+  const { username, email, password, walletAddress } = req.body;
+
+  try {
+    const user = new User({
+      username,
+      email,
+      password,
+      walletAddress
+    });
+
+    await user.save();
+
+    res.json({
+      message: "User Registered",
+      user
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email, password });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid Credentials"
+      });
+    }
+
+    res.json({
+      message: "Login Successful",
+      user
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
