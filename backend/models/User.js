81aab3a810c5009067a3618fab696b76d0ebe8d6
yoyosharjeel621-collab <yoyosharjeel621@gmail.com>
2026-02-24 12:25:59 +0500

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  walletAddress: String
+});
+
+module.exports = mongoose.model("User", UserSchema);
